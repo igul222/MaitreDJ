cdaebbffb5e640cff535ca738f6a8455a20799a5
Zainul Shah <zan2434@gmail.com>
2013-01-05 16:16:35 -0500

    logging the event with a label and inspect

--- app/controllers/push_controller.rb
+++ app/controllers/push_controller.rb
@@ -2,10 +2,10 @@ require 'uri'
 class PushController < ApplicationController
 	def index
 		@event=JSON.parse(URI.unescape(params[:checkin]))
-		logger.info("Event:"+@event)
+		logger.info("Event:"+@event.inspect)
 		#@event=JSON.parse(request.body.read)
 		checkin_id=@event[:id]
-		logger.info("ID"+checkin_id)
+		logger.info("ID"+checkin_id.inpsect)
 		uri = URI.parse("https://api.foursquare.com/v2/checkins/"+checkin_id+"/reply")
 
 		# Shortcut
