665aa2249e1a4a716dd0df33b7d832de22ec0af9
Zainul Shah <zan2434@gmail.com>
2013-01-05 16:36:27 -0500

    changed to full https request

--- app/controllers/push_controller.rb
+++ app/controllers/push_controller.rb
@@ -9,9 +9,17 @@ class PushController < ApplicationController
 		uri = URI.parse("https://api.foursquare.com/v2/checkins/"+checkin_id+"/reply")
 
 		# Shortcut
-		#response = Net::HTTP.post_form(uri, {"CHECKIN_ID" => checkin_id, "url" => new_song_url})
+		req = Net::HTTP::Post.new(uri.path)
+		req.set_form_data({'CHECKIN_ID'=>checkin_id, 'url'=>url})
+		http = Net::HTTP.new(uri.host, uri.port)
+		http.use_ssl = true
+		response = http.request(req)
+		logger.info("Response:"+response.inspect)
+
 		logger.info("CHECKIN_ID:"+checkin_id.inspect)
 		logger.info("URI:"+uri.inspect)
 		logger.info("URL:"+new_song_url.inspect)
+
+		render :text => 'OK'
 	end
 end
