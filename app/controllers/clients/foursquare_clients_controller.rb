d56277facbf7d968f2b41897376b8cdb751ec401
Ishaan Gulrajani <igul222@gmail.com>
2013-01-05 18:15:53 -0500

    stuff

--- app/controllers/clients/foursquare_clients_controller.rb
+++ app/controllers/clients/foursquare_clients_controller.rb
@@ -10,16 +10,16 @@ class Clients::FoursquareClientsController < ApplicationController
 	def callback
 		if params[:error]
 			debugger
-			Logger.error(params[:error])
+			logger.error(params[:error])
 		end
 		token = client.auth_code.get_token params[:code], redirect_uri: callback_foursquare_clients_url
 		user = FoursquareUser.find_or_create_by_access_token(token.token)
 
 		Rails.cache.write(user.foursquare_id, token.token)
 		
-		Logger.info 'WRITTEN TO GLOBAL HACKS'
-		Logger.info user.foursquare_id 
-		Logger.info token.token
+		logger.info 'WRITTEN TO GLOBAL HACKS'
+		logger.info user.foursquare_id 
+		logger.info token.token
 
 		session[:user_id] = user.id
 		redirect_to user_path
