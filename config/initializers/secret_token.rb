f4ec4aa9721f697edbbc8d5c487ea0f0dd3970c9
Zainul Shah <zan2434@gmail.com>
2013-01-05 12:36:08 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maitredj::Application.config.secret_token = 'fd45adc44cb98e5a31ffc785333869e1e976f1f63a52345b0a5ead7614ff53e71e26d3513596fbc0c33f776793b221f3fd6b7776f0ebb59326c87a69f4f03001'
