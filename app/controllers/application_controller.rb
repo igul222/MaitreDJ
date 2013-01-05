796134d8a05a48ddcb92d8a623488ace9110c4ea
Zainul Shah <zan2434@gmail.com>
2013-01-05 15:16:41 -0500

    made event variable global

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  def default_url_options
+    if Rails.env.production?
+      {:host => "maitredj.herokuapp.com"}
+    else  
+      {:host => "localhost:3000"}
+    end
+  end
 end
--- app/controllers/push_controller.rb
+++ app/controllers/push_controller.rb
@@ -1,7 +1,7 @@
 class PushController < ApplicationController
 	def index
-		event=JSON.parse(request.body.read)
-		CHECKIN_ID=event.CHECKIN_ID
+		@event=JSON.parse(request.body.read)
+		CHECKIN_ID=@event.CHECKIN_ID
 		uri = URI.parse("https://api.foursquare.com/v2/checkins/"+CHECKIN_ID+"/reply")
 
 		# Shortcut
