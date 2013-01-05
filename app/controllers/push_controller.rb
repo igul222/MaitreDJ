75a7cd36aa4abf55ec13ec96147e55c8fa114c67
Zainul Shah <zan2434@gmail.com>
2013-01-05 15:31:55 -0500

    changed checkin_id from constant to variable

--- app/controllers/push_controller.rb
+++ app/controllers/push_controller.rb
@@ -1,10 +1,10 @@
 class PushController < ApplicationController
 	def index
 		@event=JSON.parse(request.body.read)
-		CHECKIN_ID=@event.CHECKIN_ID
-		uri = URI.parse("https://api.foursquare.com/v2/checkins/"+CHECKIN_ID+"/reply")
+		checkin_id=@event.CHECKIN_ID
+		uri = URI.parse("https://api.foursquare.com/v2/checkins/"+checkin_id+"/reply")
 
 		# Shortcut
-		response = Net::HTTP.post_form(uri, {"CHECKIN_ID" => CHECKIN_ID, "url" => new_song_url})
+		response = Net::HTTP.post_form(uri, {"CHECKIN_ID" => checkin_id, "url" => new_song_url})
 	end
 end
