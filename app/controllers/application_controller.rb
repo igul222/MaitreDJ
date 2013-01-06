03a77942574f7ef72aa1f06e3b8ad06d3dc14426
Ishaan Gulrajani <igul222@gmail.com>
2013-01-05 19:45:28 -0500

    stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   def default_url_options
     if Rails.env.production?
-      {:host => "maitredj.herokuapp.com"}
+      {:host => "www.jamsesh.in"}
     else  
       {:host => "localhost:3000"}
     end
