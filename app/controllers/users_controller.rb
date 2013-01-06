7a9796b722609d1e6c47856031ac268da68760b8
Ishaan Gulrajani <igul222@gmail.com>
2013-01-05 19:00:14 -0500

    stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def new
-    render text: "<a href='#{new_foursquare_client_path}'><img src='https://playfoursquare.s3.amazonaws.com/press/logo/connect-black.png' /></a>"
   end
 
   def show
